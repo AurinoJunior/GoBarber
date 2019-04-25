8157a3f786c552f9e34978c6324775dc55d606ce
Aurino <aurino_aj@hotmail.com>
2019-04-25 19:16:02 -0300

    Configuring database

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'dev',
+  password: 'dev123',
+  database: 'gobarber',
+  operatorAliases: false,
+  define: {
+    timestamps: true
+  }
+}
