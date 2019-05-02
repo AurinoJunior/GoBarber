b0fd35f6ea88178ba89002ee9ee94444cf0e3872
Aurino Geraldo <aurino_aj@hotmail.com>
2019-05-01 21:27:51 -0300

    Authentication of user

---
+++ src/app/controller/SessionController.js
@@ -0,0 +1,20 @@
+const { User } = require("../models");
+
+class SessionController {
+  async login(req, res) {
+    const { email, password } = req.body;
+
+    console.log(req.body);
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user || !(await user.checkPassword(password))) {
+      console.log("Usuario ou senha incorretos");
+      return res.redirect("/");
+    }
+
+    return res.redirect("/app/dashboard");
+  }
+}
+
+module.exports = new SessionController();
--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,20 @@ const upload = require("multer")(multerConfig); // Criando instancia a partir da
 const routes = express.Router();
 
 const UserController = require("./app/controller/UserController");
+const SessionController = require("./app/controller/SessionController");
+
+routes.get("/", (req, res) => {
+  return res.render("auth/signin");
+});
 
 routes.get("/signup", (req, res) => {
   return res.render("auth/signup");
 });
 
+routes.post("/signin", SessionController.login);
 routes.post("/signup", upload.single("avatar"), UserController.create);
 
+routes.get("/app/dashboard", (req, res) => {
+  res.render("dashboard");
+});
 module.exports = routes;
