e4a704227fe696817aeca182c33b4d31a44776c4
Aurino Geraldo <aurino_aj@hotmail.com>
2019-05-07 00:08:36 -0300

    Available Controller

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ const SessionController = require("./app/controller/SessionController");
 const DashboardController = require("./app/controller/DashboardController");
 const FileController = require("./app/controller/FileController");
 const AppointmentController = require("./app/controller/AppointmentController");
+const AvailableController = require("./app/controller/AvailableController");
 
 const authMiddleware = require("./app/middlewares/auth");
 const guestMiddleware = require("./app/middlewares/guest");
@@ -38,5 +39,6 @@ routes.get("/app/logout", SessionController.destroy);
 routes.get("/app/dashboard", DashboardController.index);
 
 routes.get("/app/appointments/new/:provider", AppointmentController.index);
+routes.get("/app/available/:provider", AvailableController.index);
 
 module.exports = routes;
